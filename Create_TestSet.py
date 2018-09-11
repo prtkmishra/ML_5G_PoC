2ac7be878341630c03adaf92bcf0a42121532c23
Prateek Mishra <42146005+prtkmishra@users.noreply.github.com>
2018-09-11 22:49:25 +0100

    added sklearn module for creating test_train set

--- Create_TestSet.py
+++ Create_TestSet.py
@@ -3,13 +3,15 @@
 ## git_pull_Datasets
 import Download_Dataset
 import numpy as np
-
-def split_train_test(data, test_ratio):
-    shuffled_indices = np.random.permutation(len(data))
-    test_set_size = int(len(data)*test_ratio)
-    test_indices = shuffled_indices[:test_set_size]
-    train_indices = shuffled_indices[test_set_size:]
-    return data.iloc[train_indices], data.iloc[test_indices]
+from sklearn.model_selection import train_test_split
+# below function commented out as sklearn used
+#def split_train_test(data, test_ratio):
+#    shuffled_indices = np.random.permutation(len(data))
+#    test_set_size = int(len(data)*test_ratio)
+#    test_indices = shuffled_indices[:test_set_size]
+#    train_indices = shuffled_indices[test_set_size:]
+#    return data.iloc[train_indices], data.iloc[test_indices]
 housing = Download_Dataset.load_housing_data()
-train_set,test_set = split_train_test(housing, 0.2)
+#train_set,test_set = split_train_test(housing, 0.2)
+train_set, test_set = train_test_split(housing, test_size = 0.2, random_state = 42)
 print(len(train_set)), "train +", len(test_set), "test"
