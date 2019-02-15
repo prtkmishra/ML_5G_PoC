db8cf38472832756f1e80f2ca221b90bfbc7818c
prateek mishra <mishra.eric@gmail.com>
2019-02-15 11:28:57 +0000

    cosmetic changes

--- audio_lib/load_audio_file.py
+++ audio_lib/load_audio_file.py
@@ -1,23 +1,21 @@
 import librosa
-import Ipython.display as ipd
+#import Ipython.display as ipd
 import matplotlib.pyplot as plt 
 import librosa.display
 
-audio_path = '../audio.wav'
+def LoadAudio():
+    audio_path = '../audio.wav'
 
-""" x = returns an audio time series as a numpy array 
-    sr = default sampling rate 
-"""
+    """ x = returns an audio time series as a numpy array 
+        sr = default sampling rate 
+    """
+    x , sr = librosa.load(audio_path)
 
-x , sr = librosa.load(audio_path)
+    """ print numpy array and default sampling rate"""
+    print("x", "sr")
 
-""" print numpy array and default sampling rate"""
-print("x", "sr")
-
-"""below line to execute in jupyter notebook for playing audio """
-#ipd.Audio(audio_path)
-
-
-"""to display the audio file plot """
-plt.figure(figsize=(14,5))
-librosa.display.waveplot(x, sr=sr)
+    """below line to execute in jupyter notebook for playing audio """
+    #ipd.Audio(audio_path)
+    """to display the audio file plot """
+    plt.figure(figsize=(14,5))
+    librosa.display.waveplot(x, sr=sr)
